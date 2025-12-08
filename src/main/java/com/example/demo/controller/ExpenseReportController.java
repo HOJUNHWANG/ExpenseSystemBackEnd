a05271395452962a88d7f429da849f347838b78e
HOJUNHWANG <adind96@gmail.com>
2025-12-08 11:55:37 -0500

    Created to receive HTTP request

---
+++ src/main/java/com/example/demo/controller/ExpenseReportController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.ExpenseReportCreateRequest;
+import com.example.demo.service.ExpenseReportService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/expense-reports")
+@RequiredArgsConstructor
+public class ExpenseReportController {
+
+    private final ExpenseReportService expenseReportService;
+
+    @PostMapping
+    public ResponseEntity<Long> create(@RequestBody ExpenseReportCreateRequest request) {
+        Long id = expenseReportService.createReport(request);
+        return ResponseEntity.ok(id);
+    }
+}

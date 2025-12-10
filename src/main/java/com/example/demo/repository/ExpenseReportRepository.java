384f83049d8876d958da3b5a718341c7ba185e77
HOJUNHWANG <adind96@gmail.com>
2025-12-10 16:28:44 -0500

    Added method(submitterID & Status)

--- src/main/java/com/example/demo/repository/ExpenseReportRepository.java
+++ src/main/java/com/example/demo/repository/ExpenseReportRepository.java
@@ -1,6 +1,7 @@
 package com.example.demo.repository;
 
 import com.example.demo.domain.ExpenseReport;
+import com.example.demo.domain.ExpenseReportStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,5 +10,5 @@ public interface ExpenseReportRepository extends JpaRepository<ExpenseReport, Lo
 
     // For searching the report that submitted by certain User
     List<ExpenseReport> findBySubmitterId(Long submitterId);
-
+    List<ExpenseReport> findBySubmitterIdAndStatus(Long submitterId, ExpenseReportStatus status);
 }
